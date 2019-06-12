e694007c47de9a3f4c814d8c0fd73e477b4f98e0
Benjamin Lu <benlumia007@gmail.com>
2019-06-12 09:49:45 -0700

    changed initiator to individuality

--- comments.php
+++ comments.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Initiator ( comments.php )
+ * Individuality ( comments.php )
  *
- * @package     Initiator
+ * @package     Individuality
  * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @author      Benjamin Lu ( https://luthemes.com )
  */
 
 ?>
@@ -23,10 +23,10 @@ if ( post_password_required() ) {
 				<?php $count = get_comments_number(); ?>
 				<?php
 				if ( '1' === $count ) {
-					printf( esc_html_x( 'One Comment', 'comments title', 'initiator' ) );
+					printf( esc_html_x( 'One Comment', 'comments title', 'individuality' ) );
 				} else {
 					// Translators: 1 = counts.
-					printf( _nx( '%1$s Comment', '%1$s Comments', absint( $count ), 'comments title', 'initiator' ), absint( number_format_i18n( $count ) ) ); // phpcs:ignore
+					printf( _nx( '%1$s Comment', '%1$s Comments', absint( $count ), 'comments title', 'individuality' ), absint( number_format_i18n( $count ) ) ); // phpcs:ignore
 				}
 				?>
 			</h2>
@@ -44,8 +44,8 @@ if ( post_password_required() ) {
 		</ol>
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { ?>
 			<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-				<div class="comment-previous"><?php previous_comments_link( '<i class="fa fa-arrow-circle-o-left"></i> ' . esc_html__( 'Older Comments', 'initiator' ) ); ?></div>
-				<div class="comment-next"><?php next_comments_link( '<i class="fa fa-arrow-circle-o-right"></i> ' . esc_html__( 'Newer Comments', 'initiator' ) ); ?></div>
+				<div class="comment-previous"><?php previous_comments_link( '<i class="fa fa-arrow-circle-o-left"></i> ' . esc_html__( 'Older Comments', 'individuality' ) ); ?></div>
+				<div class="comment-next"><?php next_comments_link( '<i class="fa fa-arrow-circle-o-right"></i> ' . esc_html__( 'Newer Comments', 'individuality' ) ); ?></div>
 			</nav>
 		<?php } ?>
 		<?php comment_form(); ?>
