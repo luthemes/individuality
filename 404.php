a372a8a8ff9482f8fb041600fccac6a37e301306
Benjamin Lu <benlumia007@gmail.com>
2019-06-12 01:08:12 -0700

    changed initiator to individuality

--- 404.php
+++ 404.php
@@ -1,23 +1,18 @@
 <?php
 /**
- * Initiator ( 404.php )
+ * Individuality ( 404.php )
  *
- * @package     Initiator
+ * @package     Individuality
  * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @author      Benjamin Lu ( https://luthemes.com )
  */
-
-use Benlumia007\Backdrop\View\View as sidebar;
 ?>
 
 <?php get_header(); ?>
 	<section id="content" class="site-content">
-		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
-			<main id="main" class="content-area">
-				<?php get_template_part( 'views/content/content', '404'); ?>
-			</main>
-			<?php sidebar::display( 'sidebar', [ 'primary' ] ); ?>
-		</div>
+		<main id="main" class="content-area">
+			<?php get_template_part( 'views/content/content', '404'); ?>
+		</main>
 	</section>
 <?php get_footer(); ?>
--- views/content/content-404.php
+++ views/content/content-404.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Initiator ( content-none.php )
+ * Individuality ( content-none.php )
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @package     Individuality
+ * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @author      Benjamin Lu ( https://luthemes.com )
  */
 
 use Benlumia007\Backdrop\Entry\Entry as entry;
@@ -17,7 +17,7 @@ use Benlumia007\Backdrop\Entry\Entry as entry;
 	<div class="entry-content">
 		<p>
 			<?php
-				printf( esc_html__( "Just kidding! It looks like you have stumbled upon a page that doesn't exist, so that means I probably broke something. To find what you are looking for, check out the most recent articles below or try a search: ", 'initiator' ) );
+				printf( esc_html__( "Just kidding! It looks like you have stumbled upon a page that doesn't exist, so that means I probably broke something. To find what you are looking for, check out the most recent articles below or try a search: ", 'individuality' ) );
 			?>
 		</p>
 		<?php get_search_form(); ?>
