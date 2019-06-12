430123555ef666f55efabdc14102b8d2b3bdefa2
Benjamin Lu <benlumia007@gmail.com>
2019-06-11 23:55:26 -0700

    changed initiator to individuality

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Initiator ( header.php )
+ * Individuality ( header.php )
  *
- * @package     Initiator
+ * @package     Individuality
  * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @author      Benjamin Lu ( https://luthemes.com )
  */
 
 use Benlumia007\Backdrop\Site\Site as site;
