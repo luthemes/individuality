3d9098218bbe76bb23236468bf3ab4f39c614973
Benjamin Lu <benlumia007@gmail.com>
2019-06-20 10:59:30 -0700

    removed admin and customize feature

--- framework/framework.php
+++ framework/framework.php
@@ -4,10 +4,10 @@
  *
  * This file is used to create a new framework instance and adds specific features to the theme.
  *
- * @package     Initiator
+ * @package     Individuality
  * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @author      Benjamin Lu ( https://luthemes.com )
  */
 
 /**
@@ -16,6 +16,3 @@
  * This will create an instance of the framework allowing you to initialize the theme.
  */
 $individuality = Benlumia007\Backdrop\Framework::get_instance();
-
-$individuality->cusomize = new Individuality\Component\Customize();
-$individuality->admin    = new Individuality\Component\Admin();
