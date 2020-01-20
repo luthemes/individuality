1a5cf57efae143bb2d34e183fc377d928104fc04
Benjamin Lu <benlumia007@gmail.com>
2020-01-20 15:23:19 -0800

    fixed display of portfolio

--- views/content/content-archive-portfolio.php
+++ views/content/content-archive-portfolio.php
@@ -14,38 +14,21 @@ use Benlumia007\Backdrop\Entry\Entry as entry;
 	<header class="entry-header">
 		<?php entry::display( 'entry-title' ); ?>
 	</header>
-	<div class="entry-content">
-		<div class="portfolio-grid alignwide">
-		<?php
-				$posts_per_page = 9;
-				$query          = new WP_Query(
-					array(
-						'post_type'      => 'portfolio',
-						'posts_per_page' => $posts_per_page,
-					)
-				);
-				if ( $query->have_posts() ) {
-					while ( $query->have_posts() ) {
-						$query->the_post();
-						if ( has_post_thumbnail() ) {
-							?>
-								<div class="portfolio-items">
-									<a href="<?php echo esc_url( get_permalink() ); ?>">
-										<?php the_post_thumbnail( 'backdrop-large-thumbnails' ); ?>
-									</a>
-									<div class="wp-caption">
-										<div class="wp-caption-text">
-											<h2 class="jetpack-portfolio-title"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_post_thumbnail_caption(); ?></a></h2>
-											<span><?php echo wptexturize( wp_strip_all_tags( get_post( get_post_thumbnail_id() )->post_content ) ); // phpcs:ignore ?></span>
-										</div>
-									</div>
-								</div>
-							<?php
-						}
-					}
-				}
-				wp_reset_postdata();
-				?>
-		</div>
-	</div>
+    <div class="entry-content">
+        <ul class="portfolio-grid alignwide">
+            <?php while (have_posts()) : the_post(); ?>
+                <li>
+                    <a href="<?php echo esc_url(get_permalink()); ?>">
+                        <?php the_post_thumbnail('backdrop-large-thumbnails'); ?>
+                    </a>
+					<div class="wp-caption">
+						<div class="wp-caption-text">
+							<h2 class="jetpack-portfolio-title"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_post_thumbnail_caption(); ?></a></h2>
+							<span><?php echo wptexturize( wp_strip_all_tags( get_post( get_post_thumbnail_id() )->post_content ) ); // phpcs:ignore ?></span>
+						</div>
+					</div>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    </div>
 </article>
