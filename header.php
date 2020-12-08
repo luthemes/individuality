8315fcedc8d566c1fd5d02b1f511288704a1526d
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-07 23:34:47 -0800

    add css class branding-navigation

--- header.php
+++ header.php
@@ -21,9 +21,11 @@
 <div id="container" class="site-container">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'initiator' ) ?></a>
 	<header id="header" class="site-header">
-		<div class="site-branding">
-			<?php Benlumia007\Backdrop\Site\display_site_title(); ?>
-			<?php Benlumia007\Backdrop\Site\display_site_description(); ?>
+		<div class="branding-navigation">
+			<div class="site-branding">
+				<?php Benlumia007\Backdrop\Site\display_site_title(); ?>
+				<?php Benlumia007\Backdrop\Site\display_site_description(); ?>
+			</div>
+			<?php Benlumia007\Backdrop\View\display( 'menu', [ 'primary' ] ); ?>
 		</div>
-		<?php Benlumia007\Backdrop\View\display( 'menu', [ 'primary' ] ); ?>
 	</header>
