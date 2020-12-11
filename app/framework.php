34f071e28eb4f446975eb0251667562bc0b161c8
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-11 13:15:17 -0800

    update files and rename project to individuality

--- app/framework.php
+++ app/framework.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Initiator ( framework.php )
+ * Individuality ( framework.php )
  *
  * This file is used to create a new framework instance and adds specific features to the theme.
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @package     Individuality
+ * @copyright   Copyright (C) 2019-2020. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
@@ -15,15 +15,16 @@
  *
  * This will create an instance of the framework allowing you to initialize the theme.
  */
-$initiator = Benlumia007\Backdrop\Framework::get_instance();
+$individuality = Benlumia007\Backdrop\Framework::get_instance();
 
-$initiator->menus = new Benlumia007\Backdrop\Menu\Menu(
+$individuality->menus = new Benlumia007\Backdrop\Menu\Menu(
 	$args = [
-		'primary' => esc_html__( 'Primary Navigation', 'initiator' ),
+		'primary' => esc_html__( 'Primary Navigation', 'individuality' ),
+		'social' => esc_html__( 'Social Navigation', 'individuality' ),
 	]
 );
 
-$initiator->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
+$individuality->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
 	$args = [
 		'primary' => [
 			'name' => esc_html__( 'Primary Sidebar', 'initiator' ),
