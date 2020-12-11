ff395aaa83b568932b143a7a31a58ac05e7b5f37
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-11 13:22:32 -0800

    initial commit

---
+++ templates/template-home.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Home Template ( template-home.php )
+ * Template Name: Home
+ *
+ * @package     Individuality
+ * @copyright   Copyright (C) 2019-2020. Benjamin Lu
+ * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+?>
+<?php get_header(); ?>
+	<section id="content" class="site-content">
+		<div id="layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
+			<main id="main" class="content-area">
+				<?php
+					if ( have_posts() ) :
+						while ( have_posts() ) : the_post();
+							Benlumia007\Backdrop\Template\get_template_part( 'content/content', get_post_format() );
+						endwhile;
+						the_posts_pagination();
+					else :
+							Benlumia007\Backdrop\Template\get_template_part( 'content/content', 'none' );
+					endif;
+				?>
+			</main>
+			<?php Benlumia007\Backdrop\View\display( 'sidebar', [ 'primary' ] ); ?>
+		</div>
+	</section>
+<?php get_footer(); ?>
