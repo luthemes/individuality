6accf46dc6dedc18a04698ccc8bb45a3b615dcbb
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-11 13:18:13 -0800

    update files and rename project to individuality

--- app/functions-scripts.php
+++ app/functions-scripts.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Initiator ( functions-scripts.php )
+ * Individuality ( functions-scripts.php )
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Individuality
+ * @copyright Copyright (C) 2019-2020. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://benjlu.com )
  */
 
 /**
  * Define namespace
  */
-namespace Initiator;
+namespace Individuality;
 
 /**
  * Enqueue Scripts and Styles
@@ -30,12 +30,12 @@ add_action(
 		 * Rather than enqueue the main stylesheet, we are going to enqueue sceen.css since all of the styles will
 		 * go here. We only need parse the information for the Theme in style.css so that it can be activated.
 		 */
-		wp_enqueue_style( 'initiator-screen', get_theme_file_uri( 'public/css/screen.css' ), array(), '1.0.0' );
+		wp_enqueue_style( 'individuality-screen', get_theme_file_uri( 'public/css/screen.css' ), array(), '1.0.0' );
 
 		/**
 		 * We will be enqueue the app.js file, which mainly be for the navigation only.
 		 */
-		wp_enqueue_script( 'initiator-app', get_theme_file_uri( 'public/js/app.js' ), array('jquery'), '1.0.0', true );
+		wp_enqueue_script( 'individuality-app', get_theme_file_uri( 'public/js/app.js' ), array('jquery'), '1.0.0', true );
 
 		/**
 		 * This allows users to comment by clicking on reply so that it gets nested.
@@ -60,13 +60,13 @@ add_action(
 				{$value}
 			}
 		";
-		wp_add_inline_style( 'initiator-style', $custom_css );
+		wp_add_inline_style( 'individuality-style', $custom_css );
 	}
 );
 
 add_action(
 	'enqueue_block_editor_assets',
 	function() {
-		wp_enqueue_style( 'initiator-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
+		wp_enqueue_style( 'individuality-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
 	}
 );
