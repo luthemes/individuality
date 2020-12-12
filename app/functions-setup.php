359e7983174cfb0332121fb61704ad037bcfad58
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-11 18:32:07 -0800

    change editor-style.css to editor.css

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -89,7 +89,7 @@ add_action( 'after_setup_theme', function() {
 		 * By adding add_theme_support( 'editor-styles' ); and add_editor_style(); to enable styles in the backend of the editor.
 		 */
 		add_theme_support( 'editor-styles' );
-		add_editor_style( 'assets/css/editor-styles.css' );
+		add_editor_style( 'public/css/editor.css' );
 
 		/**
 		 * By adding add_theme_support( 'align-wide' );. This will enable alignwide and alignfull.
