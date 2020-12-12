0155a38e4969cb5c325b8548a0816e08e69f16bb
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-11 17:07:03 -0800

    initial commit

---
+++ public/views/content/content-home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Individuality ( content-single.php )
+ *
+ * @package   Individuality
+ * @copyright Copyright (C) 2018-2019. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://benjlu.com )
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+</article>
