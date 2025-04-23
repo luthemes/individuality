5381da3406e076a0c0b96075984d7ca47c47660c
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-23 22:56:51 +0000

    initial commit

---
+++ config/layouts.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Global Layouts Config.
+ *
+ * Configuration for the theme's global layouts.
+ *
+ * @package   Individuality
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/individuality
+ */
+
+return [
+    
+	'full' => [
+		'label' => __( 'Full', 'individuality' )
+	],
+	'wide' => [
+		'label' => __( 'Wide', 'individuality' )
+	],
+];
