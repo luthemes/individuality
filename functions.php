63204b2940c269a5ace98f4eb85e99471b6e2b80
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-23 00:08:32 +0000

    initial commit

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Default functions template
+ *
+ * This file is used to bootstrap the theme.
+ *
+ * @package   Individuality
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/individuality
+ */
+
+/** ------------------------------------------------------------------------------------------
+ * Bootstrap the theme.
+ * -------------------------------------------------------------------------------------------
+ * Load the bootstrap files. Note that autoload should happen first so that any classes or
+ * functions are available that we might need.
+ */
+
+require_once get_parent_theme_file_path( 'app/bootstrap-autoload.php' );
+require_once get_parent_theme_file_path( 'app/framework.php' );
