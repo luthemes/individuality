421e65eaeb5054a4e158c083a91ad28d9d6cb8ef
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-24 00:20:10 +0000

    initial commit

---
+++ app/Tools/Config.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Config Class.
+ *
+ * A simple class for grabbing and returning a configuration file from `/config`.
+ *
+ * @package   Individuality
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2025 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/individuality
+ */
+
+namespace Individuality\Tools;
+
+/**
+ * Config class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Config {
+
+	/**
+	 * Includes and returns a given PHP config file. The file must return
+	 * an array.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $name
+	 * @return array
+	 */
+	public static function get( $name ) {
+
+		$file = static::path( "{$name}.php" );
+
+		return ( array ) apply_filters( "individuality/config/{$name}/", file_exists( $file ) ? include( $file ) : [] );
+	}
+
+	/**
+	 * Returns the config path or file path if set.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $file
+	 * @return string
+	 */
+	public static function path( $file = '' ) {
+
+		$file = trim( $file, '/' );
+
+		return get_theme_file_path( $file ? "config/{$file}" : 'config' );
+	}
+}
